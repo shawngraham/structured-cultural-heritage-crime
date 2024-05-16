07e74bbc6587a4194f5cbb6d6858c6b653095b48
Shawn Graham <shawn.graham@carleton.ca>
2024-05-16 15:29:09 -0400

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -6,22 +6,22 @@ DEFAULT_BASE_PROMPT = """
 Extract information regarding cultural heritage crime from the following text.
 
 In particular, please provide the following information:
-- A list of triplets, where each triplet consists of two entities and the relation between them. Be sure to use only the predefined relations listed below and provide detailed descriptions for each entity.
+- A list of triplets, where each triplet consists of two entities and the relation between them. Only use the predefined relations listed below and provide detailed descriptions for each entity.
 
 The predefined relations are:
-- 'is_the_owner_of'
-- 'works_with'
-- 'works_for'
-- 'has_possession_of'
-- 'purchases'
-- 'buys_from'
-- 'sells'
-- 'donates_to'
-- 'obtains_from'
-- 'comes_from'
-- 'has_immediate_family_member'
-- 'legal_status_change'
-- 'has_role'
+- 'is_the_owner_of': Denotes a business relationship where an actor controls, or is the legal owner of, a business, gallery, auction house, or other for-profit organization.
+- 'works_with': Denotes a business relationship between actors who are dealers, organizations, looters, or collectors.
+- 'works_for': Describes an employment or contractual relationship between actors who are dealers, organizations, businesses, museums, government agencies, looters, or collectors.
+- 'has_possession_of': Describes a situation where a dealer, organization, collector, or auction house controls an artefact whether through ownership or other means.
+- 'purchases': Describes a situation where a dealer, organization, collector, or auction house buys an artefact.
+- 'buys_from': Describes a situation where a dealer, organization, collector, or auction house buys an artefact from a named actor.
+- 'sells': Describes a situation where a dealer, organization, collector, or auction house sells an artefact.
+- 'donates_to': Describes a situation where a dealer, organization, collector, or auction house donates an artefact to another entity.
+- 'obtains_from': Describes a situation where a dealer, organization, collector, or auction house obtains an artefact from another entity under unclear circumstances.
+- 'comes_from': Describes a situation where the provenance of an artefact is attributed.
+- 'has_immediate_family_member': Describes a direct familial relationship between two individuals.
+- 'legal_status_change': Describes when an organization, business, person, etc., has come to the attention of law enforcement.
+- 'has_role': Describes the role or roles an actor or organization can have.
 
 {format_instructions}
 
@@ -29,9 +29,7 @@ Make sure to provide a valid and well-formatted JSON adhering to the given schem
 
 Content:
 {content}
-
 """
-
 def create_prompt(
         base_prompt: str,
         output_format: PydanticOutputParser,
