72f2f6635f931ece560a6e6d03b09955643cdad2
Shawn Graham <shawn.graham@carleton.ca>
2024-05-16 15:20:53 -0400

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,17 +1,28 @@
-"""`prompt.py`"""
-
 from langchain_core.messages import BaseMessage
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
 
 DEFAULT_BASE_PROMPT = """
-Extract information regarding cultural heritage crime from the following text. 
+Extract information regarding cultural heritage crime from the following text.
 
 In particular, please provide the following information:
-- A list of entities involved in the text, including their types and detailed descriptions.
-- A list of relations that define how these entities interact with one another.
-- A list of triplets that describe specific relational configurations between these entities as head, relation, tail, providing detailed contextual explanations.
- 
+- A list of triplets, where each triplet consists of two entities and the relation between them. Be sure to use only the predefined relations listed below and provide detailed descriptions for each entity.
+
+The predefined relations are:
+- 'is_the_owner_of'
+- 'works_with'
+- 'works_for'
+- 'has_possession_of'
+- 'purchases'
+- 'buys_from'
+- 'sells'
+- 'donates_to'
+- 'obtains_from'
+- 'comes_from'
+- 'has_immediate_family_member'
+- 'legal_status_change'
+- 'has_role'
+
 {format_instructions}
 
 Make sure to provide a valid and well-formatted JSON adhering to the given schema.
