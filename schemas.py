288fd0679bfd8d289135b8286df987509eb01080
Shawn Graham <shawn.graham@carleton.ca>
2024-05-16 14:36:51 -0400

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -8,10 +8,7 @@ class Entity(BaseModel):
     """Entity schema"""
     name: str = Field(
         description="The name of the entity, representing various aspects or participants in the cultural heritage and art crime domain.",
-        examples=["actor", "collection", "policy", "place", "artefact", "organization", "museum", "government_agency", "business", "auction_house", "culture_group"]
-    )
-    description: str = Field(
-        description=("'actor' A PERSON who plays a role in the cultural heritage or art crime domain. Actors ARE ALWAYS referred by their Forename Surname, e.g, 'Doe' becomes 'John Doe'. ALL PERSONS ARE ACTORS FIRST AND FOREMOST."
+        examples=["'actor' A PERSON who plays a role in the cultural heritage or art crime domain. Actors ARE ALWAYS referred by their Forename Surname, e.g, 'Doe' becomes 'John Doe'. ALL PERSONS ARE ACTORS FIRST AND FOREMOST."
 "'collection' A named collection of antiquities. Collections ARE ALWAYS referred to by their formal name."
 "'policy' A government or organizational scheme to set a framework around aspects of the art or cultural heritage domain."
 "'place' A geographic locale where an event occurs or an object comes from or an organization etc is located."
@@ -27,17 +24,14 @@ class Entity(BaseModel):
 "'dealer' A role subordinate to ACTOR or ORGANIZATION, describing a PERSON or ENTITY involved in the buying and selling of artefacts."
 "'collector' A role subordinate to ACTOR or ORGANIZATION, describing a PERSON or ENTITY that acquires artefacts for personal or institutional collections."
 "'looter' A role subordinate to ACTOR, describing a PERSON who illegally excavates or steals cultural heritage artefacts."
-"'officer' A role subordinate to ACTOR, describing a PERSON who enforces laws or regulations related to cultural heritage, such as police officers or customs agents, or holds a managerial role in a museum.'"),
-    )
+"'officer' A role subordinate to ACTOR, describing a PERSON who enforces laws or regulations related to cultural heritage, such as police officers or customs agents, or holds a managerial role in a museum.'"
+    ]),
 
 class Relation(BaseModel):
     """Relation schema"""
     name: str = Field(
         description="The specific name of the relationship that can exist between entities in the domain.",
-        examples=["is_the_owner_of", "works_with", "works_for", "has_possession_of", "purchases", "buys_from", "sells", "donates_to", "obtains_from", "comes_from", "has_immediate_family_member", "legal_status_change", "has_role"]
-    )
-    description: str = Field(
-        description=("'is_the_owner_of' Denotes a business relationship where an ACTOR controls, or is the legal owner of, a business, gallery, auction house, or other for-profit organization.'"
+        examples=["'is_the_owner_of' Denotes a business relationship where an ACTOR controls, or is the legal owner of, a business, gallery, auction house, or other for-profit organization.'"
 "'works_with'  Denotes a business relationship between ACTORS who are dealers, organizations, looters, or collectors.'"
 "'works_for' Describes an employment or contractual relationship between ACTORS who are dealers, organizations, businesses, museums, government agencies, looters, or collectors.'"
 "'has_possession_of' Describes a situation where a dealer, organization, collector, or auction house controls an artefact whether through ownership or other means.'"
@@ -49,8 +43,8 @@ class Relation(BaseModel):
 "'comes_from' Describes a situation where the provenance of an artefact is attributed, e.g., a 5th century black-figure vase comes from classical Greece.'"
 "'has_immediate_family_member' Describes a direct familial relationship between two individuals, including relationships by blood (e.g., parent, child, sibling) and by legal commitments such as marriage (e.g., spouse). Excludes more distant blood relationships and informal relationships like common-law partnerships UNLESS that relationship is explicitly mentioned.'"
 "'legal_status_change' Describes when an organization, business, person, or so on, has come to the attention of law enforcement as when e.g., a person is charged with theft, an organization is sanctioned, an arrest warrant is issued.'"
-"'has_role' Describes the role or roles an actor or organization can have, such as SCHOLAR, DEALER, COLLECTOR, LOOTER, or OFFICER.' "),
-    )
+"'has_role' Describes the role or roles an actor or organization can have, such as SCHOLAR, DEALER, COLLECTOR, LOOTER, or OFFICER.' "
+    ]),
 
 class Pattern(BaseModel):
     """Pattern schema"""
